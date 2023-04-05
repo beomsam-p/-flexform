7cb0b4e2317a6010c3628c06cb0322a6a9e0b86e
bumsam.pyeon <bumsam.pyeon@quryon.com>
2023-04-05 16:03:35 +0900

    feat: backend 프로젝트 세팅

---
+++ backend/src/test/java/com/flexform/api/FlexformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexform.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
