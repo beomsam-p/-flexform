6eb3b4aaf996f6bf6db8e7ef9af6fb8b8717c7c8
bumsam.pyeon <bumsam.pyeon@quryon.com>
2023-04-08 01:33:12 +0900

    feat: 워크스페이스 컨트롤러를 작성한다.

--- backend/src/main/java/com/flexform/api/controller/WorkspaceController.java
+++ backend/src/main/java/com/flexform/api/controller/WorkspaceController.java
@@ -1,2 +1,34 @@
-package com.flexform.api.controller;public class WorkspaceController {
+package com.flexform.api.controller;
+
+import com.flexform.api.dto.ResponseContainer;
+import com.flexform.api.dto.StatusDto;
+import com.flexform.api.dto.WorkspaceDto;
+import com.flexform.api.service.workspace.UserService;
+import com.flexform.api.service.workspace.WorkspaceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1")
+public class WorkspaceController {
+    private final WorkspaceService workspaceService;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/workspaces")
+    public ResponseEntity<ResponseContainer> getWorkspaces() {
+        final List<WorkspaceDto> workspaces = workspaceService.getWorkspaces();
+        return ResponseEntity.ok().body(ResponseContainer.builder()
+                .status(StatusDto.builder()
+                        .code(HttpStatus.OK.value())
+                        .message(HttpStatus.OK.name())
+                        .build())
+                .data(workspaces)
+                .build());
+    }
 }
