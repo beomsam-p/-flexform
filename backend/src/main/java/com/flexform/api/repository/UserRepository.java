7d7f9a4471e77e7cbb2f74d09d21fba2ab570fcd
bumsam.pyeon <bumsam.pyeon@quryon.com>
2023-04-08 01:34:54 +0900

    freat: User Repository를 작성한다

--- backend/src/main/java/com/flexform/api/repository/UserRepository.java
+++ backend/src/main/java/com/flexform/api/repository/UserRepository.java
@@ -1,2 +1,13 @@
-package com.flexform.api.repository;public interface UserRepository {
+package com.flexform.api.repository;
+
+import com.flexform.api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
--- backend/src/main/java/com/flexform/api/repository/WorkspaceRepository.java
+++ backend/src/main/java/com/flexform/api/repository/WorkspaceRepository.java
@@ -1,2 +1,16 @@
-package com.flexform.api.repository;public interface WorkspaceRepository {
+package com.flexform.api.repository;
+
+import com.flexform.api.entity.Workspace;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface WorkspaceRepository extends JpaRepository<Workspace, UUID> {
+    @Query("SELECT w FROM Workspace w INNER JOIN FETCH w.user u WHERE u.userId = :userId")
+    List<Workspace> findWorkspacesByUserId(@Param("userId") UUID id);
 }
