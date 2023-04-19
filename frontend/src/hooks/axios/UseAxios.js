c2b81d2cfdc8018fc493cd8b0e1b5402403cb957
pbs <pbs@pbsui-iMac.local>
2023-04-19 21:39:41 +0900

    feat: axios를 custom hooks로 만들어 사용한다

--- frontend/src/hooks/axios/UseAxios.js
+++ frontend/src/hooks/axios/UseAxios.js
@@ -1,28 +1,28 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { handleApiException } from 'exception/ApiException';
 
-const useAxios = (url, method, handleError, options) => {
+import { useEffect, useState } from 'react';
+import { toCamelCase } from 'util/ConvertConvention';
+
+const useAxios = ({ url, method, body, headers }, { isCamelResponse = true, menual = false } = {}) => {
   const [response, setResponse] = useState(null);
   const [isError, setError] = useState(false);
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(true);
 
-  const excution = async () => {
-    setLoading(true);
-    console.log('async 내부 isLoading:', isLoading);
-
+  const excution = async body => {
     try {
-      const { data } = await axios[method](url, options);
-      setResponse(data);
+      const { data } = await axios[method](url, body, { headers });
+      const _data = isCamelResponse ? toCamelCase(data) : data;
+      setResponse(_data.data);
     } catch (e) {
       setError(true);
-      handleError && handleError(e);
+      handleApiException(e);
     } finally {
       setLoading(false);
     }
   };
   useEffect(() => {
-    excution();
-    console.log('isLoading', isLoading);
+    !menual && excution(body);
   }, []);
 
   return [{ response, isLoading, isError }, excution];
