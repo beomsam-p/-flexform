d79e5eda7f2649ea9e779c2ebaf6461b5f325cfd
pbs <pbs@pbsui-iMac.local>
2023-04-19 21:39:14 +0900

    feat: api 요청 시 예외 핸들링을 위한 함수를 작성한다

--- frontend/src/exception/ApiException.js
+++ frontend/src/exception/ApiException.js
@@ -1,11 +1,8 @@
-export const handleApiException = ({
-  response: {
-    data: { status },
-  },
-}) => {
-  switch (status.code) {
+export const handleApiException = response => {
+  console.log(response);
+  switch (response.status.code) {
     case 404:
-      console.error(status.message);
+      console.error(response.status.message);
       break;
     default:
       console.error('알 수 없는 에러 발생');
