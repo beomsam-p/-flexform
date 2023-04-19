ee671c5209ad2c6b6f62043d78aac27f87f93485
pbs <pbs@pbsui-iMac.local>
2023-04-19 21:37:14 +0900

    feat: @RestControllerAdvice로 예외를 핸들링한다.

--- backend/src/main/java/com/flexform/api/exception/DefaultNetworkAdvice.java
+++ backend/src/main/java/com/flexform/api/exception/DefaultNetworkAdvice.java
@@ -5,13 +5,15 @@ import com.flexform.api.dto.StatusDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class DefaultNetworkAdvice {
     @ExceptionHandler({NoHandlerFoundException.class})
-    public ResponseEntity<ResponseContainer> noHandlerFoundException(Exception e) {
+    public ResponseEntity<ResponseContainer> notFound(Exception e) {
         StatusDto statusDto = StatusDto.builder()
                 .code(HttpStatus.NOT_FOUND.value())
                 .message(e.getMessage())
@@ -23,7 +25,7 @@ public class DefaultNetworkAdvice {
     }
 
     @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
-    public ResponseEntity<ResponseContainer> httpRequestMethodNotSupportedException(Exception e) {
+    public ResponseEntity<ResponseContainer> methodNotAllowed(Exception e) {
         StatusDto statusDto = StatusDto.builder()
                 .code(HttpStatus.METHOD_NOT_ALLOWED.value())
                 .message(e.getMessage())
@@ -32,5 +34,17 @@ public class DefaultNetworkAdvice {
                 .status(statusDto)
                 .build();
         return new ResponseEntity<>(responseContainer, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler({MissingServletRequestParameterException.class})
+    public ResponseEntity<ResponseContainer> badRequest(Exception e) {
+        StatusDto statusDto = StatusDto.builder()
+                .code(HttpStatus.BAD_REQUEST.value())
+                .message(e.getMessage())
+                .build();
+        ResponseContainer responseContainer = ResponseContainer.builder()
+                .status(statusDto)
+                .build();
+        return new ResponseEntity<>(responseContainer, HttpStatus.BAD_REQUEST);
     }
 }
