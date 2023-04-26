ebaa019a3aafda81da6e24ec8d1b891939d23c1f
pbs <pbs@pbsui-iMac.local>
2023-04-26 20:01:07 +0900

    feat: 모달관련 코드를 추가한다.

--- frontend/src/components/CommonCss.js
+++ frontend/src/components/CommonCss.js
@@ -1,11 +1,11 @@
 import styled, { css } from 'styled-components';
 
-export const ThumbnailBoxCss = css`
+export const BoxCss = css`
   border: 1px solid rgba(200, 200, 200, 0.1);
   border-radius: 13px;
   box-shadow: rgba(200, 200, 200, 0.3) 2px 4px 10px;
-  cursor: pointer;
 `;
+
 export const ThumbnailContainer = styled.div`
   display: flex;
   width: 250px;
@@ -13,6 +13,6 @@ export const ThumbnailContainer = styled.div`
   padding: 0;
   background-color: rgba(250, 250, 250, 0.7);
   padding: 10px 20px;
-
-  ${ThumbnailBoxCss}
+  cursor: pointer;
+  ${BoxCss}
 `;
---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from 'styled-components';
+import { BoxCss } from './CommonCss';
+import { Button, Divider, Space } from 'antd';
+
+const Dim = styled.div`
+  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.1);
+  z-index: 999;
+`;
+
+const ModalContainer = styled.div`
+  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  ${props => console.log(props)};
+  width: ${({ modal_withd }) => modal_withd};
+  height: ${({ modal_height }) => modal_height};
+  background-color: white;
+  font-size: 20px;
+  z-index: 999;
+  ${BoxCss}
+`;
+
+const ModalHeader = styled.div`
+  margin: 25px 20px 0 20px;
+  text-align: center;
+`;
+
+const ModalBody = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px 10px 20px;
+`;
+const ModalTail = styled.div`
+  display: flex;
+  justify-content: right;
+  padding: 0 20px 20px 20px;
+`;
+
+const Modal = ({ isOpen, onSubmit, onCancle, title, body, tail, size = { width: '300px', height: '200px' } }) => {
+  console.log(size);
+  return (
+    <>
+      <Dim isOpen={isOpen} onClick={onCancle}></Dim>
+      <ModalContainer isOpen={isOpen} modal_withd={size.width} modal_height={size.height}>
+        <ModalHeader>{title}</ModalHeader>
+        <Divider />
+        <ModalBody>{body}</ModalBody>
+        <Divider />
+        <ModalTail>
+          {tail ? (
+            { tail }
+          ) : (
+            <Space wrap>
+              <Button type="primary" onClick={onSubmit}>
+                확인
+              </Button>
+              <Button onClick={onCancle}>취소</Button>
+            </Space>
+          )}
+        </ModalTail>
+      </ModalContainer>
+    </>
+  );
+};
+
+export default Modal;
